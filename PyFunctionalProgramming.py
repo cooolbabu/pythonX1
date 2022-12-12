bb23d3cfdc829f52093bad1d292253b83bf374b9
cooolbabu <cooolbabu@yahoo.com>
2022-12-12 12:07:23 -0600

    added args and kargs

--- PyFunctionalProgramming.py
+++ PyFunctionalProgramming.py
@@ -1,10 +1,25 @@
 from functools import reduce
 
+## *args and *kwargs
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
+print('-- Finished keyword args')
+
+## Processing a dictionary
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+result = [num + 3 for num in numbers if num % 2 == 0]
+print(result)
+print('-- Finished processing dictionary')
+
+
 #1 Capitalize all of the pet names and print the list
 my_pets = ['sisi', 'bibi', 'titi', 'carla', 'maya']
 
 def caps(item):
-       return item.capitalize()
+    return item.capitalize()
 
 print(list(map(caps,my_pets)))
 
