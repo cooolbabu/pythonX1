2dcf3efa852adb7a913ad9222f477d2818f285e0
cooolbabu <cooolbabu@yahoo.com>
2022-12-28 18:09:30 -0600

    Some more stuff

--- PyFunctionalProgramming.py
+++ PyFunctionalProgramming.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 ## map, filter, reduce
-#1 MAP function - Capitalize all of the pet names and print the list
+#1 MAP function
 my_pets = ['sisi', 'bibi', 'titi', 'carla', 'maya']
 
 def caps(item):
@@ -9,7 +9,21 @@ def caps(item):
 
 print(list(map(caps,my_pets))) ## Map takes a function and iterable as parameters
 
+#3 Filter
+scores = [73, 20, 65, 19, 76, 100, 88]
 
+def filterScores(item):
+    return item > 50
+
+print(list(filter(filterScores, scores)))
+
+#4 Zip function- if there is an extra .. it's omitted
+player =["John", "Sam", "Jack", "Tom", "Mary", "Art", "Nick", "JoAnn"]
+print(list(zip(player,scores)))
+
+#Lambda programming
+print(list(map(lambda pet: pet.capitalize(), my_pets)))
+print((lambda pet: pet.capitalize(), my_pets))
 
 ## *args and *kwargs
 
@@ -23,26 +37,15 @@ print('-- Finished keyword args')
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 result = [num + 3 for num in numbers if num % 2 == 0]
 print(result)
+my_list4 = [num**2 for num in range(0,100) if num%2 ==0]
+print(my_list4)
 print('-- Finished processing dictionary')
-
-
-
-
-
 
 #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5,4,3,2,1]
 
 print(list(zip(my_strings, my_numbers)))
-
-#3 Filter the scores that pass over 50%
-scores = [73, 20, 65, 19, 76, 100, 88]
-
-def filterScores(item):
-    return item > 50
-
-print(list(filter(filterScores, scores)))
 
 
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
